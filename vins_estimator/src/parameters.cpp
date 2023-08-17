ea019bc2ac33880c2eaeec20b188ed0eca4009ce
Ther <1329438302@qq.com>
2023-08-17 14:01:13 +0000

    chore: modify config

--- vins_estimator/src/parameters.cpp
+++ vins_estimator/src/parameters.cpp
@@ -58,7 +58,7 @@ void readParameters(ros::NodeHandle &n)
 
     std::string OUTPUT_PATH;
     fsSettings["output_path"] >> OUTPUT_PATH;
-    VINS_RESULT_PATH = OUTPUT_PATH + "/vins_result_no_loop.csv";
+    VINS_RESULT_PATH = OUTPUT_PATH + "/vins_result_no_loop.txt";
     std::cout << "result path " << VINS_RESULT_PATH << std::endl;
     std::ofstream fout(VINS_RESULT_PATH, std::ios::out);
     fout.close();
