a2dd102ed603f1b5252e70a5ba1dd5b29be04fc6
Ther <1329438302@qq.com>
2023-08-30 13:05:04 +0000

    chore: add LM algorithm

--- utils/include/ChipCholeskySolver.h
+++ utils/include/ChipCholeskySolver.h
@@ -36,6 +36,9 @@ public:
 private:
     in_chip_output_type in_chip(in_chip_input_type a); // in chip conversion
     Eigen::Matrix<matrix_type, -1, -1> CRSMatrix2EigenMatrix(ceres::CRSMatrix *jacobian_crs_matrix); // convert sparse matrix to Eigen matrix
+    void scaling_jacobian(Eigen::Matrix<matrix_type, -1, -1> &jacobian); // scaling jacobian
+    void positive_definitize(Eigen::Matrix<matrix_type, -1, -1> &A); // positive definitize a half positive definite matrix, for LM algorithm
+    void update_u_v(double rho, std::vector<double>& prev_parameter_blocks, std::vector<double*>& parameter_blocks); // update u and v for LM algorithm
     void cholesky_decomposition(Eigen::Matrix<matrix_type, -1, -1> &A); // cholesky decomposition
     void cholesky_solve(Eigen::Matrix<matrix_type, -1, -1> &L, Eigen::Matrix<residual_type, -1, 1> &b); // cholesky solve
 
@@ -45,6 +48,10 @@ private:
     double parameter_tolerance;
     double function_tolerance;
     double gradient_tolerance;
+
+    double u;
+    double v;
+    double jacobian_scaling_factor;
 };
 
 #endif // CHOLESKY_SOLVER_H
--- utils/src/ChipCholeskySolver.cpp
+++ utils/src/ChipCholeskySolver.cpp
@@ -8,14 +8,18 @@ std::string Summary::brief_report()
     report += "Termination type: " + termination_type + "\n";
     report += "Max gradient norm: " + std::to_string(max_gradient_norm) + "\n";
     report += "Cost change ratio: " + std::to_string(cost_change_ratio) + "\n";
-    report += "Parameter change ratio: " + std::to_string(parameter_change_ratio) + "\n";
+    report += "Parameter change ratio: " + std::to_string(parameter_change_ratio);
     return report;
 }
 
 template <bool chip_arithmetic, bool print_flag,
           typename in_chip_input_type, typename in_chip_output_type,
           typename residual_type, typename matrix_type>
-ChipCholeskySolver<chip_arithmetic, print_flag, in_chip_input_type, in_chip_output_type, residual_type, matrix_type>::ChipCholeskySolver(ceres::Problem &problem, int max_iteration, double parameter_tolerance, double function_tolerance, double gradient_tolerance) : problem_(problem), iteration_(max_iteration), parameter_tolerance(parameter_tolerance), function_tolerance(function_tolerance), gradient_tolerance(gradient_tolerance) {}
+ChipCholeskySolver<chip_arithmetic, print_flag, in_chip_input_type, in_chip_output_type, residual_type, matrix_type>::ChipCholeskySolver(ceres::Problem &problem, int max_iteration, double parameter_tolerance, double function_tolerance, double gradient_tolerance) : problem_(problem), iteration_(max_iteration), parameter_tolerance(parameter_tolerance), function_tolerance(function_tolerance), gradient_tolerance(gradient_tolerance)
+{
+    u = 0;
+    v = 2; // for levenberg-marquardt algorithm
+}
 
 template <bool chip_arithmetic, bool print_flag,
           typename in_chip_input_type, typename in_chip_output_type,
@@ -92,12 +96,72 @@ Eigen::Matrix<matrix_type, -1, -1> ChipCholeskySolver<chip_arithmetic, print_fla
 template <bool chip_arithmetic, bool print_flag,
           typename in_chip_input_type, typename in_chip_output_type,
           typename residual_type, typename matrix_type>
+void ChipCholeskySolver<chip_arithmetic, print_flag, in_chip_input_type, in_chip_output_type, residual_type, matrix_type>::scaling_jacobian(Eigen::Matrix<matrix_type, -1, -1> &jacobian)
+{
+    if (u == 0)
+    {
+        jacobian_scaling_factor = 1.0 / (1.0 + jacobian.colwise().norm().maxCoeff());
+    }
+    jacobian = jacobian_scaling_factor * jacobian;
+}
+
+template <bool chip_arithmetic, bool print_flag,
+          typename in_chip_input_type, typename in_chip_output_type,
+          typename residual_type, typename matrix_type>
+void ChipCholeskySolver<chip_arithmetic, print_flag, in_chip_input_type, in_chip_output_type, residual_type, matrix_type>::positive_definitize(Eigen::Matrix<matrix_type, -1, -1> &A)
+{
+    if (u == 0) // initialize the u with the max diagonal element
+    {
+        u = A.diagonal().maxCoeff();
+    }
+    const matrix_type min_diagonal = 1e-6;
+    const matrix_type max_diagonal = 1e32;
+    for (int i = 0; i < A.rows(); ++i)
+    {
+        matrix_type tmp = std::sqrt(u * (std::min)((std::max)(A(i, i), min_diagonal), max_diagonal));
+        A(i, i) += tmp * tmp;
+    }
+}
+
+template <bool chip_arithmetic, bool print_flag,
+          typename in_chip_input_type, typename in_chip_output_type,
+          typename residual_type, typename matrix_type>
+void ChipCholeskySolver<chip_arithmetic, print_flag, in_chip_input_type, in_chip_output_type, residual_type, matrix_type>::update_u_v(double rho, std::vector<double>& prev_parameter_blocks, std::vector<double*>& parameter_blocks)
+{
+    if (rho > 0) // accept the update
+    {
+        u = u * std::max(1.0 / 3, 1 - std::pow(2 * rho - 1, 3));
+        v = 2;
+    }
+    else // reject the update
+    {
+        u = u * v;
+        v = 2 * v;
+        for (auto it = parameter_blocks.begin(); it != parameter_blocks.end(); it++)
+        {
+            *(*it) = prev_parameter_blocks[it - parameter_blocks.begin()];
+        }
+    }
+}
+
+template <bool chip_arithmetic, bool print_flag,
+          typename in_chip_input_type, typename in_chip_output_type,
+          typename residual_type, typename matrix_type>
 void ChipCholeskySolver<chip_arithmetic, print_flag, in_chip_input_type, in_chip_output_type, residual_type, matrix_type>::cholesky_decomposition(Eigen::Matrix<matrix_type, -1, -1> &A)
 {
     int n = A.rows();
     for (int i = 0; i < n; i++)
     {
-        in_chip_input_type tmp = std::sqrt(A(i, i));
+        in_chip_input_type tmp;
+        try
+        {
+            tmp = std::sqrt(A(i, i));
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << e.what() << ", "
+                      << "Cholesky decomposition failed. Check whether the matrix is positive definite." << '\n';
+        }
         if (chip_arithmetic)
         {
             A(i, i) = in_chip(tmp);
@@ -109,13 +173,14 @@ void ChipCholeskySolver<chip_arithmetic, print_flag, in_chip_input_type, in_chip
         for (int j = i + 1; j < n; j++)
         {
             in_chip_input_type t;
-            if (A(i, i) == 0)
-            {
-                t = 0;
-            }
-            else
+            try
             {
                 t = in_chip_input_type(A(j, i) / A(i, i));
+            }
+            catch (const std::exception &e)
+            {
+                std::cerr << e.what() << ", "
+                          << "Cholesky decomposition failed. Check whether the matrix is positive definite." << '\n';
             }
             if (chip_arithmetic)
             {
@@ -173,10 +238,10 @@ template <bool chip_arithmetic, bool print_flag,
 Summary ChipCholeskySolver<chip_arithmetic, print_flag, in_chip_input_type, in_chip_output_type, residual_type, matrix_type>::solve()
 {
     std::vector<double *> parameter_blocks;
-    
+
     std::vector<double> residuals;
     std::vector<double> gradients;
-    double prev_cost = 0, cost = 0;
+    double new_cost = 0, cost = 0;
     ceres::CRSMatrix jacobian_crs_matrix;
     ceres::Problem::EvaluateOptions eval_opts;
 
@@ -196,23 +261,32 @@ Summary ChipCholeskySolver<chip_arithmetic, print_flag, in_chip_input_type, in_c
     for (int i = 0; i < iteration_; i++)
     {
         // deep copy parameter_blocks to prev_parameter_blocks
-
-        prev_cost = cost;
-
         problem_.Evaluate(eval_opts, &cost, &residuals, &gradients, &jacobian_crs_matrix);
 
         // get J and H
         J = CRSMatrix2EigenMatrix(&jacobian_crs_matrix);
+
+        // scaling jacobian
+        scaling_jacobian(J);
+
         H = J.transpose() * J;
-        
+
+        // regularize H
+        positive_definitize(H);
+
         // cholesky decomposition
         cholesky_decomposition(H);
 
         // get b
-        Eigen::Matrix<residual_type, -1, 1> b = Eigen::Map<Eigen::Matrix<residual_type, -1, 1>>(gradients.data(), gradients.size());
+        Eigen::Matrix<residual_type, -1, 1> b = J.transpose() * Eigen::Map<Eigen::VectorXd>(residuals.data(), residuals.size()); 
+        // deep copy b to prev_b
+        Eigen::Matrix<matrix_type, -1, 1> prev_b = b;
 
         // solve
         cholesky_solve(H, b);
+
+        // scaling b(in pair with scaling jacobian)
+        Eigen::Matrix<residual_type, -1, 1> b_scale = b * jacobian_scaling_factor;
 
         std::vector<double> prev_parameter_blocks;
         for (auto it = parameter_blocks.begin(); it != parameter_blocks.end(); it++)
@@ -223,42 +297,16 @@ Summary ChipCholeskySolver<chip_arithmetic, print_flag, in_chip_input_type, in_c
         // update x
         for (auto it = parameter_blocks.begin(); it != parameter_blocks.end(); it++)
         {
-            *(*it) -= b(it - parameter_blocks.begin());
+            *(*it) -= b_scale(it - parameter_blocks.begin());
         }
 
-        // if (print_flag)
-        // {
-        //     std::cout << "[iteration: " << i << "]" << std::endl;
-        //     double max_gradient_norm = std::abs(*std::max_element(gradients.begin(), gradients.end(), [](double a, double b)
-        //                                                 { return std::abs(a) < std::abs(b); }));
-        //     std::cout << "max gradient norm: ";
-        //     std::cout << max_gradient_norm << std::endl;
-        //     std::cout << "|cost_change|/cost: " << std::abs(cost - prev_cost) / prev_cost << std::endl;
-
-        //     // get the change of parameter_blocks
-        //     std::vector<double> parameter_blocks_change;
-        //     for (auto it = parameter_blocks.begin(); it != parameter_blocks.end(); it++)
-        //     {
-        //         parameter_blocks_change.push_back(*(*it) - prev_parameter_blocks[it - parameter_blocks.begin()]);
-        //     }
-        //     // compute the norm of parameter_blocks_change
-        //     double parameter_blocks_change_norm = std::sqrt(std::inner_product(parameter_blocks_change.begin(), parameter_blocks_change.end(), parameter_blocks_change.begin(), 0.0));
-        //     // std::cout << "parameter_blocks_change_norm: " << parameter_blocks_change_norm << std::endl;
-        //     double parameter_blocks_norm = std::sqrt(std::inner_product(prev_parameter_blocks.begin(), prev_parameter_blocks.end(), prev_parameter_blocks.begin(), 0.0));
-        //     // std::cout << "parameter_blocks_norm: " << parameter_blocks_norm << std::endl;
-        //     std::cout << "norm ratio: " << parameter_blocks_change_norm / parameter_blocks_norm << std::endl;
-        //     std::cout << "x: ";
-        //     for (auto it = parameter_blocks.begin(); it != parameter_blocks.end(); it++)
-        //     {
-        //         std::cout << *(*it) << " ";
-        //     }
-        //     std::cout << std::endl;
-        // }
+        // calculate the new cost(notice that cost is 0.5 * ||residuals||^2)
+        problem_.Evaluate(eval_opts, &new_cost, nullptr, nullptr, nullptr);
 
         double max_gradient_norm = std::abs(*std::max_element(gradients.begin(), gradients.end(), [](double a, double b)
-                                                    { return std::abs(a) < std::abs(b); }));
-        double cost_change_ratio = std::abs(cost - prev_cost) / prev_cost;
-        
+                                                              { return std::abs(a) < std::abs(b); }));
+        double cost_change_ratio = std::abs(new_cost - cost) / cost;
+
         std::vector<double> parameter_blocks_change;
         for (auto it = parameter_blocks.begin(); it != parameter_blocks.end(); it++)
         {
@@ -267,6 +315,17 @@ Summary ChipCholeskySolver<chip_arithmetic, print_flag, in_chip_input_type, in_c
         double parameter_blocks_change_norm = std::sqrt(std::inner_product(parameter_blocks_change.begin(), parameter_blocks_change.end(), parameter_blocks_change.begin(), 0.0));
         double prev_parameter_blocks_norm = std::sqrt(std::inner_product(prev_parameter_blocks.begin(), prev_parameter_blocks.end(), prev_parameter_blocks.begin(), 0.0));
         double parameter_change_ratio = parameter_blocks_change_norm / prev_parameter_blocks_norm;
+
+        double cost_change = 2 * (cost - new_cost);
+        std::cout << "cost:" << cost << std::endl;
+        std::cout << "new_cost:" << new_cost << std::endl;
+        std::cout << "cost_change:" << cost_change << std::endl;
+        double model_cost_change = b.dot(2 * prev_b - J.transpose() * J * b);
+        std::cout << "model_cost_change:" << model_cost_change << std::endl;
+
+        // update u and v
+        double rho = cost_change / model_cost_change;
+        update_u_v(rho, prev_parameter_blocks, parameter_blocks);
 
         if (max_gradient_norm < gradient_tolerance)
         {
@@ -303,7 +362,7 @@ Summary ChipCholeskySolver<chip_arithmetic, print_flag, in_chip_input_type, in_c
                             .max_gradient_norm = max_gradient_norm,
                             .cost_change_ratio = cost_change_ratio,
                             .parameter_change_ratio = parameter_change_ratio};
-            
+
             return summary;
         }
     }
